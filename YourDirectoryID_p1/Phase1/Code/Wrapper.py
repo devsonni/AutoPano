210335271ab39a2a6e8eade60f51038ba2c8c92c
Dev Soni <devsoni44.pp@gmail.com>
2024-06-02 20:36:42 -0400

    Weighted mask for blending added -- working

--- YourDirectoryID_p1/Phase1/Code/Wrapper.py
+++ YourDirectoryID_p1/Phase1/Code/Wrapper.py
@@ -26,6 +26,7 @@ import random
 import math
 import glob
 
+
 # Reading imgs form the given directory, this will be replaced in server implementation
 def img_reading(folder):
     image_list = os.listdir(folder)
@@ -35,177 +36,18 @@ def img_reading(folder):
     for file in image_list:
         file_path = os.path.join(folder, file)
         img = cv2.imread(file_path)
+        y, x, c = np.shape(img)
+        img = cv2.resize(img, (int(y/2), int(x/2)))
         if img is not None:
             imgs.append(img)
 
     n_imgs = len(image_list)
     print(f"Number of imgs Loaded: {n_imgs}")
     
-    return imgs, n_imgs  
-
-# Corner Detection, Save Corner detection output as corners.png
-def corner_detection(InputPath, imgs, n_imgs):
-    InputPath = str(InputPath) + '/CornerDetection'
-    if not os.path.exists(InputPath):
-        os.makedirs(InputPath)        
-    
-    corner_coords_list = []
-    num_corners_list = []
-    c_scores_list = []
-
-    for i in range(n_imgs):
-        img = imgs[i]
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
- 
-        gray = np.float32(gray)
-        dst = cv2.cornerHarris(gray,3,5,0.1)
-
-        # Result is dilated for marking the corners, not important
-        dst = cv2.dilate(dst,None)
-
-        # Extracting the coordinates of corner points in img
-        threshold = 0.001 * dst.max()
-        
-        # Discarding weak points
-        dst[dst <= threshold] = 0
-        c_scores_list.append(dst)
-
-        # Extracting the coordinates of the corners
-        corner_coords = np.argwhere(dst > 0)
-        corners = [(int(x), int(y)) for y, x in corner_coords]
-        num_corners = len(corners)
-        corner_coords_list.append(corners)
-        num_corners_list.append(num_corners)
-
-        # Creating a copy of the image
-        img_copy = img.copy()
-        # Applying red points to the detected corners
-        img_copy[dst > threshold] = [0, 0, 255]
-
-        # Saving the imgs
-        file_name = str(i+1) + '.png'
-        output_file_path = os.path.join(InputPath, file_name)
-        cv2.imwrite(output_file_path, img_copy)
-
-    return corner_coords_list, num_corners_list, c_scores_list
-
-
-# ANMS: Adaptive Non-Maximal Suppression
-def ANMS(imgs, n_imgs, corner_coords_list, num_corners_list, c_score_list, InputPath, N_best=1000):
-
-    InputPath = str(InputPath) + '/ANMS'
-    if not os.path.exists(InputPath):
-        os.makedirs(InputPath)
-    
-	# Return
-    selected_point_list = []
-
-    # Iterating through each imgs
-    for ig in range(n_imgs):
-        # computing the regional peak maxima 
-        coords = peak_local_max(c_score_list[ig], min_distance=2, threshold_abs=0.01)
-        r = np.full(len(coords)-1, np.inf)
-        for i in range(len(coords)-1):
-            for j in range(len(coords)-1):
-                if c_score_list[ig][coords[i][0], coords[i][1]] < c_score_list[ig][coords[j][0], coords[j][1]]:
-                    ED = np.sqrt((coords[i][0] - coords[j][0]) ** 2 + (coords[i][1] - coords[j][1]) ** 2)
-                    if ED < r[i]:
-                         r[i] = ED
-        
-        # Sort the distances in descending order
-        sorted_indices = np.argsort(r)[::-1]
-        N_best = min(N_best, len(coords))
-        selected_indices = sorted_indices[:N_best]
-
-        # Extracting coordinates of the selected points
-        selected_pts = np.zeros((N_best, 2))
-        for i in range(N_best-1):
-            selected_pts[i] = coords[selected_indices[i]]
-        selected_pts = selected_pts.astype(int)
-
-        selected_point_list.append(selected_pts)
-
-        # Creating a copy of the image
-        img_copy = imgs[ig].copy()
-        
-        # plotting the points on img
-        for i in range(N_best):
-             cv2.circle(img_copy, (selected_pts[i][1], selected_pts[i][0]), 2, (0, 0, 255), -1)
-
-        # Saving the imgs
-        file_name = str(ig+1) + '.png'
-        output_file_path = os.path.join(InputPath, file_name)
-        cv2.imwrite(output_file_path, img_copy)
-
-    return selected_point_list
-
-# Feature Descriptors (FDs)
-def FD(imgs, n_imgs, n_best_pts):
-    FDs= []
-    F_coords = []
-    # Going through each imgs
-    for ig in range(n_imgs):
-        img = imgs[ig].copy()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = np.float32(img)
-        FDi = []
-        Coords = []
-        # creating path of 41x41 to the main img
-        for j in range(len(n_best_pts[ig])):
-            x1, y1 = n_best_pts[ig][j][0]-20, n_best_pts[ig][j][1]-20
-            x2, y2 = n_best_pts[ig][j][0]+21, n_best_pts[ig][j][1]+21
-            s_y, s_x = np.shape(img)
-            if x1 > 0 and y1 > 0 and x2 < s_x and y2 < s_y:
-                patch_center = [n_best_pts[ig][j][0], n_best_pts[ig][j][1]]
-                patch = img[x1:x2, y1:y2]
-                patch = cv2.GaussianBlur(patch,(7,7),cv2.BORDER_DEFAULT)
-                patch = cv2.resize(patch, (8,8))
-                patch = cv2.resize(patch, (64,1))
-                feature_vector = abs(np.mean(patch) - patch)/np.std(patch)
-                FDi.append(feature_vector)
-                Coords.append(patch_center)
-        FDs.append(FDi)
-        F_coords.append(Coords)
-    return FDs, F_coords
-
-# Feature Descriptors (FDs) Using of SIFT
-def Features_(InputPath, imgs, n_imgs):
-    # Creating folder to save imgs
-    InputPath = str(InputPath) + '/SIFT'
-    if not os.path.exists(InputPath):
-        os.makedirs(InputPath)
-
-    FDs = []
-    for i in range(n_imgs):
-        img = imgs[i].copy()
-        img2 = imgs[i].copy()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = np.float32(img)
-
-        sift = cv2.SIFT_create()
-
-        # Convert corners to KeyPoint objects
-        # keypoints = [cv2.KeyPoint(x=float(x), y=float(y), size=1) for (x, y) in n_best_pts[i]]
-
-        # Compute the SIFT descriptors at the given keypoints
-        keypoints, descriptors = sift.detectAndCompute(imgs[i], None)
-        # Convert keypoints to (x, y) coordinates
-        keypoints_coords = np.array([kp.pt for kp in keypoints])
-
-        FDs.append(descriptors)
-
-        for j in range(len(keypoints_coords)):
-            cv2.circle(img2, (int(keypoints_coords[j][0]), int(keypoints_coords[j][1])), 2, (0, 0, 255), -1)
-        
-        # saving the drawmatch
-        file_name = str(i+1) + '.png'
-        output_file_path = os.path.join(InputPath, file_name)
-        cv2.imwrite(output_file_path, img2)
-    
-    return FDs, 
+    return imgs, n_imgs
 
 # New Feature Matching -- 2nd version
-def match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline):
+def match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline=True):
     """
     Match features between two images using SIFT descriptors,
     draw the matches, and find the homography matrix.
@@ -215,32 +57,41 @@ def match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline):
     :return: Image containing matches and the homography matrix.
     """
     InputPath2 = InputPath
-    # Creating folder to save imgs
-    if not pipeline:
+    InputPath3 = InputPath
+
+    # This is for saving best match
+    if pipeline:
         InputPath2 = str(InputPath2) + '/FMatch'
+        print(InputPath2)
         if not os.path.exists(InputPath2):
             os.makedirs(InputPath2)
+    
+    # This is for saving feature descriptor
+    if pipeline:
+        InputPath3 = str(InputPath3) + '/FD'
+        print(InputPath3)
+        if not os.path.exists(InputPath3):
+            os.makedirs(InputPath3)
     
     Hs = []
     
     for i in range(n_imgs-1):
         img = imgs[i].copy()
         img2 = imgs[i+1].copy()
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # img = np.float32(img)
-        # img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        # img2 = np.float32(img2)
         
         # Create SIFT detector
         sift = cv2.SIFT_create()
-        
-        print(img.shape)
-        print(img2.shape)
 
         # Detect keypoints and compute descriptors for both images
         kp1, des1 = sift.detectAndCompute(img, None)
         kp2, des2 = sift.detectAndCompute(img2, None)
         
+        # Draw keypoints on the image
+        img_key = img
+        img_key_2 = img2
+        img_key = cv2.drawKeypoints(img, kp1, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        img_key_2 = cv2.drawKeypoints(img2, kp2, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
         # Create a Brute-Force Matcher object
         bf = cv2.BFMatcher()
         
@@ -264,110 +115,44 @@ def match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline):
         H, _ = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 7, 0.99)
         
         Hs.append(H)
+        
+        # saving the features
+        if pipeline:
+            file_name = str(i+1) + '.png'
+            output_file_path = os.path.join(InputPath3, file_name)
+            cv2.imwrite(output_file_path, img_key)
+            file_name = str(i+2) + '.png'
+            output_file_path = os.path.join(InputPath3, file_name)
+            cv2.imwrite(output_file_path, img_key_2)
 
         # saving the drawmatch
-        if not pipeline:
+        if pipeline:
             file_name = str(i+1) + '.png'
             output_file_path = os.path.join(InputPath2, file_name)
             cv2.imwrite(output_file_path, img_matches)
 
     return Hs
 
-# Feature Matching
-def FM(InputPath, imgs, n_imgs, Features, F_Coords):
-    # Creating folder to save imgs
-    InputPath = str(InputPath) + '/FM'
-    if not os.path.exists(InputPath):
-        os.makedirs(InputPath)
-    
-    # Matching features to each pair of the imgs -> loop - pairs, features1, all features2 
-    feature_match = []
-    for ig in range(n_imgs-1):
-        f_match = []
-        for i in range(len(Features[ig])):
-            sq_dist = []
-            for j in range(len(Features[ig+1])):
-                dist = np.sum(np.sqrt((Features[ig][i] - Features[ig+1][j])**2))
-                sq_dist.append(dist)
-            
-            sorted_indices = np.argsort(sq_dist)
-            if len(sorted_indices) > 1 and np.isfinite(sq_dist[sorted_indices[1]]) and sq_dist[sorted_indices[1]] != 0:
-                if sq_dist[sorted_indices[0]] / sq_dist[sorted_indices[1]] < 0.7:
-                    f_match.append((F_Coords[ig][i], F_Coords[ig+1][sorted_indices[0]]))
-        
-        # Appending match point coordinates to the list
-        feature_match.append(f_match)
+# Weighted mask for blending the images
+def create_weighted_mask(mask):
+    # Ensure the mask is single-channel and of type uint8
+    if len(mask.shape) == 3:
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    if mask.dtype != np.uint8:
+        mask = mask.astype(np.uint8)
 
-    # Drawing the image match
-    for i in range(len(feature_match)):
-        # merging two imgs
-        img_copy = np.concatenate((imgs[i], imgs[i+1]), axis=1)
+    # Compute distance transform
+    dist_transform = cv2.distanceTransform(mask, cv2.DIST_L2, 3)
+    # Normalize the distance transform to [0, 1]
+    max_dist = np.max(dist_transform)
+    if max_dist > 0:
+        weighted_mask = dist_transform / max_dist
+    else:
+        weighted_mask = dist_transform
 
-        # Writing custom drawing
-        for (x1, y1), (x2, y2) in feature_match[i]:
-            cv2.circle(img_copy, (int(y1), int(x1)), 1, (0, 0, 255), 1)  # Red border circle
-            cv2.circle(img_copy, (int(y2 + imgs[i].shape[1]), int(x2)), 1, (0, 0, 255), 1)
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # Random color
-            cv2.line(img_copy, (y1, x1), (y2 + imgs[i].shape[1], x2), color, 1)
+    return weighted_mask
 
-        # saving the drawmatch
-        file_name = str(i+1) + '.png'
-        output_file_path = os.path.join(InputPath, file_name)
-        cv2.imwrite(output_file_path, img_copy)
-
-    return feature_match
-
-
-# Estimate Homography, and RANSAC
-def RanHomo(InputPath, imgs, n_imgs, Matching, reprojection_threshold=2, max_iters=5000, confidence=0.8):
-    # Creating folder to save imgs
-    InputPath = str(InputPath) + '/RANSAC'
-    if not os.path.exists(InputPath):
-        os.makedirs(InputPath)
-    
-    Hs = []
-    Inliers_ = []
-    masks = []
-    # looping through each pairs
-    for ig in range(n_imgs-1):
-        src_pts = np.float32([match[0] for match in Matching[ig]]).reshape(-1, 1, 2)
-        dst_pts = np.float32([match[1] for match in Matching[ig]]).reshape(-1, 1, 2)
-    
-        H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, reprojection_threshold, maxIters=max_iters, confidence=confidence)
-    
-        inliers = mask.ravel().tolist()
-
-        Hs.append(H)
-        Inliers_.append(inliers)
-        masks.append(mask)
-
-    filtered_matches_ = []
-    for ig in range(n_imgs-1):
-        filtered_matches = [match for match, inlier in zip(Matching[ig], inliers) if inlier]
-        filtered_matches_.append(filtered_matches)
-    
-    # Drawing the matches on the imgs
-    for i in range(len(filtered_matches_)):
-        # merging two imgs
-        img_copy = np.concatenate((imgs[i], imgs[i+1]), axis=1)
-
-        # Writing custom drawing
-        for (x1, y1), (x2, y2) in filtered_matches_[i]:
-            cv2.circle(img_copy, (y1, x1), 1, (0, 0, 255), 1)  # Red border circle
-            cv2.circle(img_copy, (y2 + imgs[i].shape[1], x2), 1, (0, 0, 255), 1)
-
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # Random color
-            cv2.line(img_copy, (y1, x1), (y2 + imgs[i].shape[1], x2), color, 1)
-
-        # saving the drawmatch
-        file_name = str(i+1) + '.png'
-        output_file_path = os.path.join(InputPath, file_name)
-        cv2.imwrite(output_file_path, img_copy)
-
-    return Hs
-
-# Wrapping and Pano
-# trying for only two images, for the first case only   
+# wrap, stitch, blend and create a single panorama image
 def Pano(imgs, n_imgs, Hs, InputPath):
     # Creating folder to save imgs
     InputPath2 = InputPath
@@ -421,7 +206,15 @@ def Pano(imgs, n_imgs, Hs, InputPath):
             mask = cv2.bitwise_or(mask1, mask2)
 
             # Combine the images
-            result = cv2.addWeighted(img1_warped, 0.5, img2_warped, 0.5, 0)
+            # result = cv2.addWeighted(img1_warped, 0.5, img2_warped, 0.5, 0)
+
+            # Create weighted masks
+            weighted_mask1 = create_weighted_mask(mask1)
+            weighted_mask2 = create_weighted_mask(mask2)
+
+            # Combine the images using the weights
+            result = (img1_warped * weighted_mask1[..., None] + img2_warped * weighted_mask2[..., None]) /\
+                (weighted_mask1[..., None] + weighted_mask2[..., None])
 
             # saving the drawmatch
             file_name = str(i) + '.png'
@@ -468,18 +261,25 @@ def Pano(imgs, n_imgs, Hs, InputPath):
             mask = cv2.bitwise_or(mask1, mask2)
 
             # Combine the images
-            result = cv2.addWeighted(img1_warped, 0.5, img2_warped, 0.5, 0)
+            # result = cv2.addWeighted(img1_warped, 0.5, img2_warped, 0.5, 0)
+            # Create weighted masks
+            weighted_mask1 = create_weighted_mask(mask1)
+            weighted_mask2 = create_weighted_mask(mask2)
+
+            # Combine the images using the weights
+            result = (img1_warped * weighted_mask1[..., None] + img2_warped * weighted_mask2[..., None]) /\
+                (weighted_mask1[..., None] + weighted_mask2[..., None])
 
             # saving the drawmatch
             file_name = str(i) + '.png'
             output_file_path = os.path.join(InputPath, file_name)
             cv2.imwrite(output_file_path, result)
 
-    # reading the intermediate panorama
+    # reading the intermediate panorama -- add it back after blending is fixed
     folder = "../Data/Train/Set" + str(InputPath2[-1]) + "/Panorama"
     image_list = os.listdir(folder)
     image_list = [item for item in image_list if os.path.isfile(os.path.join(folder, item))]
-    print(image_list)
+    print("Stitching: ", image_list)
     image_list.sort()
     intermediate = []
     for file in image_list:
@@ -492,24 +292,16 @@ def Pano(imgs, n_imgs, Hs, InputPath):
     # Delete only files, not directories
     if not len(intermediate) == 1:
         [os.remove(f) for f in image_files if os.path.isfile(f)]
-        
-    print("after delete:" + str(os.listdir(folder)))
+
+    # print("Stitching: " + str(os.listdir(folder)))
     for i in range(len(intermediate)-1):
-        Pipeline(InputPath2, intermediate, len(intermediate))
+        Pipeline(InputPath2, intermediate, len(intermediate), pipeline=False)
 
-        
 
-def Blending():
-    pass
+def Pipeline(InputPath, imgs, n_imgs, pipeline=True):
 
-def Pipeline(InputPath, imgs, n_imgs):
-
-    H = match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline=1)
-    # Feature Matching
-    # Matching = FM(InputPath, imgs, n_imgs, Features, n_best_pts)
-
-    # RANSAC and Estimating Homography
-    # H = RanHomo(InputPath, imgs, n_imgs, Matching)
+    # gettign homography
+    H = match_features_and_find_homography(InputPath, imgs, n_imgs, pipeline)
 
     # Panorama
     Pano(imgs, n_imgs, H, InputPath)
@@ -521,29 +313,9 @@ def main():
     parser.add_argument('--Path', default="../Data/Train/Set1", help="Enter the folder name to imgs from your directory")
     Args = parser.parse_args()
     InputPath = Args.Path
-    NumFeatures = Args.NumFeatures
 
     # Loading the imgs
     imgs, n_imgs = img_reading(InputPath)
-    
-    # Corner detection
-    # corner_coords_list, num_corners_list, c_score_list = corner_detection(InputPath, imgs, n_imgs)
-    
-    # ANMS
-    # n_best_pts = ANMS(imgs, n_imgs, corner_coords_list, num_corners_list, c_score_list, InputPath)
-
-    # Feature Descriptors
-    # Features = Features_(InputPath, imgs, n_imgs)
-
-    # H = match_features_and_find_homography(InputPath, imgs, n_imgs)
-    # # Feature Matching
-    # # Matching = FM(InputPath, imgs, n_imgs, Features, n_best_pts)
-
-    # # RANSAC and Estimating Homography
-    # # H = RanHomo(InputPath, imgs, n_imgs, Matching)
-
-    # # Panorama
-    # Pano(imgs, n_imgs, H, InputPath)
 
     Pipeline(InputPath, imgs, n_imgs)
 
